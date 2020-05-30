435037d3bfb4f637c184aa4617421ef2f2d32b56
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-05-31 05:11:12 +0530

    code clean up

--- actions/fetchFromEpicStore.js
+++ actions/fetchFromEpicStore.js
@@ -2,30 +2,12 @@ import puppeteer from "puppeteer";
 import cheerio from "cheerio";
 import axios from "axios";
 import fs from "fs";
-import mkdirp from "mkdirp";
 
 export async function fetchFromEpicStore() {
     try {
-        const filePath = "temp/results.json";
-        mkdirp("temp");
-
         const source = "https://www.epicgames.com";
         const url =
             "https://www.epicgames.com/store/en-US/browse?sortBy=releaseDate&sortDir=DESC&pageSize=1000";
-
-        if (fs.existsSync(filePath)) {
-            const existingData = fs.readFileSync("temp/results.json");
-            if (existingData) {
-                const parsedValue = JSON.parse(existingData);
-                if (
-                    parsedValue &&
-                    new Date(parsedValue.expiryTime).getTime() <
-                        new Date().getTime()
-                ) {
-                    return existingData.records;
-                }
-            }
-        }
 
         const browser = await puppeteer.launch({
             args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -33,9 +15,8 @@ export async function fetchFromEpicStore() {
         });
 
         const page = await browser.newPage();
-        await page.goto(url, {
-            waitUntil: "networkidle0",
-        });
+
+        await page.goto(url);
 
         let bodyHTML = await page.evaluate(() => document.body.innerHTML);
 
@@ -63,46 +44,29 @@ export async function fetchFromEpicStore() {
             const imageContainer = $(imageSelector, elem);
             const nameContainer = $(nameSelector, elem);
 
-            if (
+            itemDetails.name =
                 nameContainer[0] &&
                 nameContainer[0].children[0] &&
-                nameContainer[0].children[0].data
-            ) {
-                itemDetails.name = nameContainer[0].children[0].data;
-            }
+                nameContainer[0].children[0].data;
 
-            if (imageContainer[0] && imageContainer[0].attribs) {
-                itemDetails.image =
-                    imageContainer[0].attribs["data-image"] || null;
-            }
+            itemDetails.image =
+                imageContainer[0] && imageContainer[0].attribs["data-image"];
 
-            if (
-                elem.children[0] &&
-                elem.children[0].attribs &&
-                elem.children[0].attribs.href
-            ) {
-                itemDetails.link = source + "" + elem.children[0].attribs.href;
-            }
+            itemDetails.link =
+                source +
+                "" +
+                (elem.children[0] &&
+                    elem.children[0].attribs &&
+                    elem.children[0].attribs.href);
 
-            if (priceContainer[0]) {
-                itemDetails.price = priceContainer[0].children[0]
-                    ? priceContainer[0].children[0].data
-                    : null;
+            itemDetails.price = priceContainer[0].children[0]
+                ? priceContainer[0].children[0].data
+                : null;
 
-                if (itemDetails.price.toLowerCase().includes("free")) {
-                    result.push(itemDetails);
-                }
+            if (itemDetails.price.toLowerCase().includes("free")) {
+                result.push(itemDetails);
             }
         });
-
-        const fileData = {
-            expiryTime: new Date(
-                new Date().setHours(new Date().getHours() + 1)
-            ),
-            records: result,
-        };
-
-        fs.writeFileSync("temp/results.json", JSON.stringify(fileData));
 
         await browser.close();
         return result;
