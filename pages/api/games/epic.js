b02b934baa05edfe9e03ce3c40437c2393b80348
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-05-31 05:08:28 +0530

    null check

--- pages/api/games/epic.js
+++ pages/api/games/epic.js
@@ -4,11 +4,10 @@ export default async (req, res) => {
   try {
     if (req.method === "GET") {
       const data = await fetchFromEpicStore();
-      res.status(200).send(data);
+      res.status(200).send(data || []);
     }
     return res.send(404).end();
   } catch (err) {
-    console.error(err);
     return res.status(500).send({ error: "Oops! Something went wrong!" });
   }
 };
