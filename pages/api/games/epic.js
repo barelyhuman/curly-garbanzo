6b38b9145575dcd373ffca98ce3e12310c36fb0f
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-05-31 16:50:59 +0530

    log erros

--- pages/api/games/epic.js
+++ pages/api/games/epic.js
@@ -11,6 +11,9 @@ export default async (req, res) => {
     }
     return res.send(404).end();
   } catch (err) {
-    return res.status(500).send({ error: 'Oops! Something went wrong!' });
+    console.error(err);
+    return res
+      .status(500)
+      .send({ error: 'Oops! Something went wrong!', message: String(err) });
   }
 };
